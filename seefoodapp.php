1f2497038e9c8bda9de1401c01494f0f136ca80c
Sam <samuel_ramirez_07@yahoo.com>
2020-07-21 16:14:54 -0700

    Version 1
    Previous SeeFood Files

---
+++ seefoodapp.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `seefoodapp`
+ */
+
+/* `seefoodapp`.`menu_items` */
+$menu_items = array(
+  array('id' => '1','restaurant' => '1','image' => 'test.jpg','name' => 'burger','description' => 'ajcnxjkszcnka','price' => '15'),
+  array('id' => '1','restaurant' => '2','image' => 'https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,format=auto,quality=50/https://cdn.doordash.com/media/photos/1b5c76b4-15c1-4263-9dd1-6a8fd5c75312-retina-large-jpeg','name' => 'Keto Salad Bowl','description' => 'Supergreens, steak, tomatillo-red salsa, cheese and guacamole.','price' => '11.35')
+);
+
+/* `seefoodapp`.`restaurant` */
+$restaurant = array(
+  array('id' => '1','name' => 'Name','address' => 'add','city' => 'marina','postal_code' => '93933'),
+  array('id' => '2','name' => 'Chipotle','address' => '560 Munras Ave #20','city' => 'Monterey','postal_code' => '93940')
+);
